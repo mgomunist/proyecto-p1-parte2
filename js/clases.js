66b9282a12607b8f80c1d3f8cdc87ede509731ef
Mariana Gómez <gomunistmariana@gmail.com>
2026-06-16 13:44:48 +0000

    clases influencer articulo y venta

--- js/clases.js
+++ js/clases.js
@@ -1,2 +1,36 @@
 /* Autores: Mariana Gómez Munist - Nro de estudiante: 207978
             Lucero Scigliano - Nro de estudiante: 292302 */
+
+class Influencer {
+    constructor(nombre, mail, comision) {
+        this.nombre = nombre;
+        this.mail = mail;
+        this.comision = comision; // número, ej: 10 (representa 10%)
+    }
+}
+
+class Articulo {
+    constructor(codigo, descripcion, precio) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.precio = precio; // número
+    }
+}
+
+class Venta {
+    constructor(numero, articulo, influencer, cantidad, medio) {
+        this.numero = numero;
+        this.articulo = articulo;       // objeto Articulo
+        this.influencer = influencer;   // objeto Influencer
+        this.cantidad = cantidad;       // número
+        this.medio = medio;             // string, ej: "1-Instagram"
+    }
+
+    calcularTotal() {
+        return this.articulo.precio * this.cantidad;
+    }
+
+    calcularComision() {
+        return this.calcularTotal() * (this.influencer.comision / 100);
+    }
+}
